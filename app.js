6f6403c88754638d1a932ee05e4bea7e17938c5f
Ivan <vsem2004@gmail.com>
2021-12-24 15:30:34 +0300

    Markup for catalog page and adding of partials hbs

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const serverRouter = require("./server/routes/main-router");
+const hbs = require('hbs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-// По поводу этого не уверен
 app.use(express.json());
 
 app.set("view engine", "hbs");
+hbs.registerPartials('./server/views/partials');
 app.set("views", "./server/views");
 
 app.use(express.static("./public"));
