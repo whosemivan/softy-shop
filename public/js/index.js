289e5bd294a0b48c7db99f3d22d71a7aa355ee10
Ivan <vsem2004@gmail.com>
2021-12-29 15:06:34 +0300

    try to adaptive menu and create cool effect in card

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,9 @@ const menu = document.querySelector('.main-nav');
 const button = document.querySelector('.header__btn');
 
 button.classList.remove('visually-hidden');
-menu.classList.add('visually-hidden');
+menu.classList.add('main-nav--hidden');
 
 button.addEventListener('click', function() {
-    menu.classList.toggle('visually-hidden');
+    menu.classList.toggle('main-nav--hidden');
     button.classList.toggle('header__btn--opened');
 });
